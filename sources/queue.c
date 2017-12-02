ffd9852a972dd460523ff223b666882f13de91d0
daddinuz <daddinuz@gmail.com>
2017-12-02 19:00:40 +0100

    naming on queue.c

--- sources/queue.c
+++ sources/queue.c
@@ -17,23 +17,23 @@
 
 typedef void *kit_Queue_Super;
 
-typedef void (*kit_Queue_Super_Trait_fnDelete)(kit_Queue_Super);
-typedef enum kit_Result (*kit_Queue_Super_Trait_fnPush)(kit_Queue_Super, void *);
-typedef enum kit_Result (*kit_Queue_Super_Trait_fnPop)(kit_Queue_Super, void **);
-typedef enum kit_Result (*kit_Queue_Super_Trait_fnBack)(kit_Queue_Super, void **);
-typedef enum kit_Result (*kit_Queue_Super_Trait_fnFront)(kit_Queue_Super, void **);
-typedef size_t (*kit_Queue_Super_Trait_fnSize)(kit_Queue_Super);
-typedef bool (*kit_Queue_Super_Trait_fnIsEmpty)(kit_Queue_Super);
+typedef void (*kit_Queue_Super_deleteFn)(kit_Queue_Super);
+typedef enum kit_Result (*kit_Queue_Super_pushFn)(kit_Queue_Super, void *);
+typedef enum kit_Result (*kit_Queue_Super_popFn)(kit_Queue_Super, void **);
+typedef enum kit_Result (*kit_Queue_Super_backFn)(kit_Queue_Super, void **);
+typedef enum kit_Result (*kit_Queue_Super_FrontFn)(kit_Queue_Super, void **);
+typedef size_t (*kit_Queue_Super_sizeFn)(kit_Queue_Super);
+typedef bool (*kit_Queue_Super_isEmptyFn)(kit_Queue_Super);
 
 struct kit_Queue {
     kit_Queue_Super super;
-    kit_Queue_Super_Trait_fnDelete mDelete;
-    kit_Queue_Super_Trait_fnPush mPush;
-    kit_Queue_Super_Trait_fnPop mPop;
-    kit_Queue_Super_Trait_fnBack mBack;
-    kit_Queue_Super_Trait_fnFront mFront;
-    kit_Queue_Super_Trait_fnSize mSize;
-    kit_Queue_Super_Trait_fnIsEmpty mIsEmpty;
+    kit_Queue_Super_deleteFn fnDelete;
+    kit_Queue_Super_pushFn fnPush;
+    kit_Queue_Super_popFn fnPop;
+    kit_Queue_Super_backFn fnBack;
+    kit_Queue_Super_FrontFn fnFront;
+    kit_Queue_Super_sizeFn fnSize;
+    kit_Queue_Super_isEmptyFn fnIsEmpty;
 };
 
 Option kit_Queue_fromDoublyList(void) {
@@ -45,13 +45,13 @@ Option kit_Queue_fromDoublyList(void) {
         superOption = kit_DoublyList_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Queue_Super_Trait_fnDelete) kit_DoublyList_delete;
-            self->mPush = (kit_Queue_Super_Trait_fnPush) kit_DoublyList_pushBack;
-            self->mPop = (kit_Queue_Super_Trait_fnPop) kit_DoublyList_popFront;
-            self->mBack = (kit_Queue_Super_Trait_fnBack) kit_DoublyList_back;
-            self->mFront = (kit_Queue_Super_Trait_fnFront) kit_DoublyList_front;
-            self->mSize = (kit_Queue_Super_Trait_fnSize) kit_DoublyList_size;
-            self->mIsEmpty = (kit_Queue_Super_Trait_fnIsEmpty) kit_DoublyList_isEmpty;
+            self->fnDelete = (kit_Queue_Super_deleteFn) kit_DoublyList_delete;
+            self->fnPush = (kit_Queue_Super_pushFn) kit_DoublyList_pushBack;
+            self->fnPop = (kit_Queue_Super_popFn) kit_DoublyList_popFront;
+            self->fnBack = (kit_Queue_Super_backFn) kit_DoublyList_back;
+            self->fnFront = (kit_Queue_Super_FrontFn) kit_DoublyList_front;
+            self->fnSize = (kit_Queue_Super_sizeFn) kit_DoublyList_size;
+            self->fnIsEmpty = (kit_Queue_Super_isEmptyFn) kit_DoublyList_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -69,13 +69,13 @@ Option kit_Queue_fromSinglyList(void) {
         superOption = kit_SinglyList_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Queue_Super_Trait_fnDelete) kit_SinglyList_delete;
-            self->mPush = (kit_Queue_Super_Trait_fnPush) kit_SinglyList_pushBack;
-            self->mPop = (kit_Queue_Super_Trait_fnPop) kit_SinglyList_popFront;
-            self->mBack = (kit_Queue_Super_Trait_fnBack) kit_SinglyList_back;
-            self->mFront = (kit_Queue_Super_Trait_fnFront) kit_SinglyList_front;
-            self->mSize = (kit_Queue_Super_Trait_fnSize) kit_SinglyList_size;
-            self->mIsEmpty = (kit_Queue_Super_Trait_fnIsEmpty) kit_SinglyList_isEmpty;
+            self->fnDelete = (kit_Queue_Super_deleteFn) kit_SinglyList_delete;
+            self->fnPush = (kit_Queue_Super_pushFn) kit_SinglyList_pushBack;
+            self->fnPop = (kit_Queue_Super_popFn) kit_SinglyList_popFront;
+            self->fnBack = (kit_Queue_Super_backFn) kit_SinglyList_back;
+            self->fnFront = (kit_Queue_Super_FrontFn) kit_SinglyList_front;
+            self->fnSize = (kit_Queue_Super_sizeFn) kit_SinglyList_size;
+            self->fnIsEmpty = (kit_Queue_Super_isEmptyFn) kit_SinglyList_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -93,13 +93,13 @@ Option kit_Queue_fromXorList(void) {
         superOption = kit_XorList_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Queue_Super_Trait_fnDelete) kit_XorList_delete;
-            self->mPush = (kit_Queue_Super_Trait_fnPush) kit_XorList_pushBack;
-            self->mPop = (kit_Queue_Super_Trait_fnPop) kit_XorList_popFront;
-            self->mBack = (kit_Queue_Super_Trait_fnBack) kit_XorList_back;
-            self->mFront = (kit_Queue_Super_Trait_fnFront) kit_XorList_front;
-            self->mSize = (kit_Queue_Super_Trait_fnSize) kit_XorList_size;
-            self->mIsEmpty = (kit_Queue_Super_Trait_fnIsEmpty) kit_XorList_isEmpty;
+            self->fnDelete = (kit_Queue_Super_deleteFn) kit_XorList_delete;
+            self->fnPush = (kit_Queue_Super_pushFn) kit_XorList_pushBack;
+            self->fnPop = (kit_Queue_Super_popFn) kit_XorList_popFront;
+            self->fnBack = (kit_Queue_Super_backFn) kit_XorList_back;
+            self->fnFront = (kit_Queue_Super_FrontFn) kit_XorList_front;
+            self->fnSize = (kit_Queue_Super_sizeFn) kit_XorList_size;
+            self->fnIsEmpty = (kit_Queue_Super_isEmptyFn) kit_XorList_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -117,13 +117,13 @@ Option kit_Queue_fromVector(void) {
         superOption = kit_Vector_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Queue_Super_Trait_fnDelete) kit_Vector_delete;
-            self->mPush = (kit_Queue_Super_Trait_fnPush) kit_Vector_pushBack;
-            self->mPop = (kit_Queue_Super_Trait_fnPop) kit_Vector_popFront;
-            self->mBack = (kit_Queue_Super_Trait_fnBack) kit_Vector_back;
-            self->mFront = (kit_Queue_Super_Trait_fnFront) kit_Vector_front;
-            self->mSize = (kit_Queue_Super_Trait_fnSize) kit_Vector_size;
-            self->mIsEmpty = (kit_Queue_Super_Trait_fnIsEmpty) kit_Vector_isEmpty;
+            self->fnDelete = (kit_Queue_Super_deleteFn) kit_Vector_delete;
+            self->fnPush = (kit_Queue_Super_pushFn) kit_Vector_pushBack;
+            self->fnPop = (kit_Queue_Super_popFn) kit_Vector_popFront;
+            self->fnBack = (kit_Queue_Super_backFn) kit_Vector_back;
+            self->fnFront = (kit_Queue_Super_FrontFn) kit_Vector_front;
+            self->fnSize = (kit_Queue_Super_sizeFn) kit_Vector_size;
+            self->fnIsEmpty = (kit_Queue_Super_isEmptyFn) kit_Vector_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -134,40 +134,40 @@ Option kit_Queue_fromVector(void) {
 
 void kit_Queue_delete(struct kit_Queue *self) {
     if (self) {
-        self->mDelete(self->super);
+        self->fnDelete(self->super);
         kit_Allocator_free(self);
     }
 }
 
 enum kit_Result kit_Queue_push(struct kit_Queue *self, void *e) {
     assert(self);
-    return self->mPush(self->super, e);
+    return self->fnPush(self->super, e);
 }
 
 enum kit_Result kit_Queue_pop(struct kit_Queue *self, void **out) {
     assert(self);
     assert(out);
-    return self->mPop(self->super, out);
+    return self->fnPop(self->super, out);
 }
 
 enum kit_Result kit_Queue_back(struct kit_Queue *self, void **out) {
     assert(self);
     assert(out);
-    return self->mBack(self->super, out);
+    return self->fnBack(self->super, out);
 }
 
 enum kit_Result kit_Queue_front(struct kit_Queue *self, void **out) {
     assert(self);
     assert(out);
-    return self->mFront(self->super, out);
+    return self->fnFront(self->super, out);
 }
 
 size_t kit_Queue_size(struct kit_Queue *self) {
     assert(self);
-    return self->mSize(self->super);
+    return self->fnSize(self->super);
 }
 
 bool kit_Queue_isEmpty(struct kit_Queue *self) {
     assert(self);
-    return self->mIsEmpty(self->super);
+    return self->fnIsEmpty(self->super);
 }
