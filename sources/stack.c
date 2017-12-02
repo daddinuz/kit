65f5beb0c037985012ff22184670212050003728
daddinuz <daddinuz@gmail.com>
2017-12-02 19:13:04 +0100

    naming on stack.c

--- sources/stack.c
+++ sources/stack.c
@@ -17,21 +17,21 @@
 
 typedef void *kit_Stack_Super;
 
-typedef void (*kit_Stack_Super_Trait_fnDelete)(kit_Stack_Super);
-typedef enum kit_Result (*kit_Stack_Super_Trait_fnPush)(kit_Stack_Super, void *);
-typedef enum kit_Result (*kit_Stack_Super_Trait_fnPop)(kit_Stack_Super, void **);
-typedef enum kit_Result (*kit_Stack_Super_Trait_fnBack)(kit_Stack_Super, void **);
-typedef size_t (*kit_Stack_Super_Trait_fnSize)(kit_Stack_Super);
-typedef bool (*kit_Stack_Super_Trait_fnIsEmpty)(kit_Stack_Super);
+typedef void (*kit_Stack_Super_deleteFn)(kit_Stack_Super);
+typedef enum kit_Result (*kit_Stack_Super_pushFn)(kit_Stack_Super, void *);
+typedef enum kit_Result (*kit_Stack_Super_popFn)(kit_Stack_Super, void **);
+typedef enum kit_Result (*kit_Stack_Super_backFn)(kit_Stack_Super, void **);
+typedef size_t (*kit_Stack_Super_sizeFn)(kit_Stack_Super);
+typedef bool (*kit_Stack_Super_isEmptyFn)(kit_Stack_Super);
 
 struct kit_Stack {
     kit_Stack_Super super;
-    kit_Stack_Super_Trait_fnDelete mDelete;
-    kit_Stack_Super_Trait_fnPush mPush;
-    kit_Stack_Super_Trait_fnPop mPop;
-    kit_Stack_Super_Trait_fnBack mBack;
-    kit_Stack_Super_Trait_fnSize mSize;
-    kit_Stack_Super_Trait_fnIsEmpty mIsEmpty;
+    kit_Stack_Super_deleteFn fnDelete;
+    kit_Stack_Super_pushFn fnPush;
+    kit_Stack_Super_popFn fnPop;
+    kit_Stack_Super_backFn fnBack;
+    kit_Stack_Super_sizeFn fnSize;
+    kit_Stack_Super_isEmptyFn fnIsEmpty;
 };
 
 Option kit_Stack_fromDoublyList(void) {
@@ -43,12 +43,12 @@ Option kit_Stack_fromDoublyList(void) {
         superOption = kit_DoublyList_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Stack_Super_Trait_fnDelete) kit_DoublyList_delete;
-            self->mPush = (kit_Stack_Super_Trait_fnPush) kit_DoublyList_pushBack;
-            self->mPop = (kit_Stack_Super_Trait_fnPop) kit_DoublyList_popBack;
-            self->mBack = (kit_Stack_Super_Trait_fnBack) kit_DoublyList_back;
-            self->mSize = (kit_Stack_Super_Trait_fnSize) kit_DoublyList_size;
-            self->mIsEmpty = (kit_Stack_Super_Trait_fnIsEmpty) kit_DoublyList_isEmpty;
+            self->fnDelete = (kit_Stack_Super_deleteFn) kit_DoublyList_delete;
+            self->fnPush = (kit_Stack_Super_pushFn) kit_DoublyList_pushBack;
+            self->fnPop = (kit_Stack_Super_popFn) kit_DoublyList_popBack;
+            self->fnBack = (kit_Stack_Super_backFn) kit_DoublyList_back;
+            self->fnSize = (kit_Stack_Super_sizeFn) kit_DoublyList_size;
+            self->fnIsEmpty = (kit_Stack_Super_isEmptyFn) kit_DoublyList_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -66,12 +66,12 @@ Option kit_Stack_fromSinglyList(void) {
         superOption = kit_SinglyList_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Stack_Super_Trait_fnDelete) kit_SinglyList_delete;
-            self->mPush = (kit_Stack_Super_Trait_fnPush) kit_SinglyList_pushFront;
-            self->mPop = (kit_Stack_Super_Trait_fnPop) kit_SinglyList_popFront;
-            self->mBack = (kit_Stack_Super_Trait_fnBack) kit_SinglyList_front;
-            self->mSize = (kit_Stack_Super_Trait_fnSize) kit_SinglyList_size;
-            self->mIsEmpty = (kit_Stack_Super_Trait_fnIsEmpty) kit_SinglyList_isEmpty;
+            self->fnDelete = (kit_Stack_Super_deleteFn) kit_SinglyList_delete;
+            self->fnPush = (kit_Stack_Super_pushFn) kit_SinglyList_pushFront;
+            self->fnPop = (kit_Stack_Super_popFn) kit_SinglyList_popFront;
+            self->fnBack = (kit_Stack_Super_backFn) kit_SinglyList_front;
+            self->fnSize = (kit_Stack_Super_sizeFn) kit_SinglyList_size;
+            self->fnIsEmpty = (kit_Stack_Super_isEmptyFn) kit_SinglyList_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -89,12 +89,12 @@ Option kit_Stack_fromXorList(void) {
         superOption = kit_XorList_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Stack_Super_Trait_fnDelete) kit_XorList_delete;
-            self->mPush = (kit_Stack_Super_Trait_fnPush) kit_XorList_pushBack;
-            self->mPop = (kit_Stack_Super_Trait_fnPop) kit_XorList_popBack;
-            self->mBack = (kit_Stack_Super_Trait_fnBack) kit_XorList_back;
-            self->mSize = (kit_Stack_Super_Trait_fnSize) kit_XorList_size;
-            self->mIsEmpty = (kit_Stack_Super_Trait_fnIsEmpty) kit_XorList_isEmpty;
+            self->fnDelete = (kit_Stack_Super_deleteFn) kit_XorList_delete;
+            self->fnPush = (kit_Stack_Super_pushFn) kit_XorList_pushBack;
+            self->fnPop = (kit_Stack_Super_popFn) kit_XorList_popBack;
+            self->fnBack = (kit_Stack_Super_backFn) kit_XorList_back;
+            self->fnSize = (kit_Stack_Super_sizeFn) kit_XorList_size;
+            self->fnIsEmpty = (kit_Stack_Super_isEmptyFn) kit_XorList_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -112,12 +112,12 @@ Option kit_Stack_fromVector(void) {
         superOption = kit_Vector_new();
         if (Option_isSome(superOption)) {
             self->super = Option_unwrap(superOption);
-            self->mDelete = (kit_Stack_Super_Trait_fnDelete) kit_Vector_delete;
-            self->mPush = (kit_Stack_Super_Trait_fnPush) kit_Vector_pushBack;
-            self->mPop = (kit_Stack_Super_Trait_fnPop) kit_Vector_popBack;
-            self->mBack = (kit_Stack_Super_Trait_fnBack) kit_Vector_back;
-            self->mSize = (kit_Stack_Super_Trait_fnSize) kit_Vector_size;
-            self->mIsEmpty = (kit_Stack_Super_Trait_fnIsEmpty) kit_Vector_isEmpty;
+            self->fnDelete = (kit_Stack_Super_deleteFn) kit_Vector_delete;
+            self->fnPush = (kit_Stack_Super_pushFn) kit_Vector_pushBack;
+            self->fnPop = (kit_Stack_Super_popFn) kit_Vector_popBack;
+            self->fnBack = (kit_Stack_Super_backFn) kit_Vector_back;
+            self->fnSize = (kit_Stack_Super_sizeFn) kit_Vector_size;
+            self->fnIsEmpty = (kit_Stack_Super_isEmptyFn) kit_Vector_isEmpty;
         } else {
             kit_Allocator_free(self);
         }
@@ -128,34 +128,34 @@ Option kit_Stack_fromVector(void) {
 
 void kit_Stack_delete(struct kit_Stack *self) {
     if (self) {
-        self->mDelete(self->super);
+        self->fnDelete(self->super);
         kit_Allocator_free(self);
     }
 }
 
 enum kit_Result kit_Stack_push(struct kit_Stack *self, void *e) {
     assert(self);
-    return self->mPush(self->super, e);
+    return self->fnPush(self->super, e);
 }
 
 enum kit_Result kit_Stack_pop(struct kit_Stack *self, void **out) {
     assert(self);
     assert(out);
-    return self->mPop(self->super, out);
+    return self->fnPop(self->super, out);
 }
 
 enum kit_Result kit_Stack_back(struct kit_Stack *self, void **out) {
     assert(self);
     assert(out);
-    return self->mBack(self->super, out);
+    return self->fnBack(self->super, out);
 }
 
 size_t kit_Stack_size(struct kit_Stack *self) {
     assert(self);
-    return self->mSize(self->super);
+    return self->fnSize(self->super);
 }
 
 bool kit_Stack_isEmpty(struct kit_Stack *self) {
     assert(self);
-    return self->mIsEmpty(self->super);
+    return self->fnIsEmpty(self->super);
 }
