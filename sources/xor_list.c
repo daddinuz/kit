b73b669b47f6849d91593e478f062b3565462e4c
daddinuz <daddinuz@gmail.com>
2018-01-18 17:39:25 +0100

    aligned xor list interface

--- sources/xor_list.c
+++ sources/xor_list.c
@@ -3,7 +3,7 @@
  *
  * Author: daddinuz
  * email:  daddinuz@gmail.com
- * Date:   November 25, 2017 
+ * Date:   January 18, 2018
  */
 
 #include <assert.h>
@@ -18,14 +18,16 @@ struct kit_XorList_Node {
 };
 
 static OptionOf(struct kit_XorList_Node *)
-kit_XorList_Node_new(void *e);
+kit_XorList_Node_new(void *element)
+__attribute__((__warn_unused_result__));
 
 static struct kit_XorList_Node *
-kit_XorList_Node_xor(struct kit_XorList_Node *p1, struct kit_XorList_Node *p2);
+kit_XorList_Node_xor(const struct kit_XorList_Node *p1, const struct kit_XorList_Node *p2)
+__attribute__((__warn_unused_result__));
 
 static void *
 kit_XorList_Node_delete(struct kit_XorList_Node *self)
-__attribute__((__nonnull__));
+__attribute__((__warn_unused_result__, __nonnull__));
 
 struct kit_XorList_Node_Pair {
     struct kit_XorList_Node *prev;
@@ -37,8 +39,8 @@ struct kit_XorList_Node_Pair {
  *  Always check for result status, do never use out without checking!!!
  */
 static ResultOf(struct kit_XorList_Node_Pair *, OutOfRangeError)
-kit_XorList_Node_Pair_fetch(struct kit_XorList *list, struct kit_XorList_Node_Pair *out, size_t index)
-__attribute__((__nonnull__));
+kit_XorList_Node_Pair_fetch(const struct kit_XorList *list, size_t index, struct kit_XorList_Node_Pair *out)
+__attribute__((__warn_unused_result__, __nonnull__));
 
 struct kit_XorList {
     int operationId;
@@ -47,29 +49,14 @@ struct kit_XorList {
     struct kit_XorList_Node *back;
 };
 
-Option kit_XorList_new(void) {
+OptionOf(struct kit_XorList *)
+kit_XorList_new(void) {
     struct kit_XorList *self;
     return kit_Allocator_calloc(1, sizeof(*self));
 }
 
-void kit_XorList_clear(struct kit_XorList *self) {
-    assert(self);
-
-    for (size_t i = self->size; i > 0; i--) {
-        Result_unwrap(kit_XorList_popFront(self));
-    }
-
-    self->operationId += 1;
-}
-
-void kit_XorList_delete(struct kit_XorList *self) {
-    if (self) {
-        kit_XorList_clear(self);
-        kit_Allocator_free(self);
-    }
-}
-
-Result kit_XorList_insert(struct kit_XorList *self, void *e, const size_t index) {
+OneOf(Ok, OutOfRangeError, OutOfMemoryError)
+kit_XorList_insert(struct kit_XorList *const self, const size_t index, void *const element) {
     assert(self);
     /*
      * A <-> B <-> C
@@ -83,12 +70,12 @@ Result kit_XorList_insert(struct kit_XorList *self, void *e, const size_t index)
     struct kit_XorList_Node *newNode = NULL;
 
     if (index > self->size) {
-        return Result_error(&OutOfRangeError);
+        return OutOfRangeError;
     }
 
-    newNodeOption = kit_XorList_Node_new(e);
+    newNodeOption = kit_XorList_Node_new(element);
     if (Option_isNone(newNodeOption)) {
-        return Result_error(&OutOfMemoryError);
+        return OutOfMemoryError;
     }
     newNode = Option_unwrap(newNodeOption);
 
@@ -107,8 +94,7 @@ Result kit_XorList_insert(struct kit_XorList *self, void *e, const size_t index)
     } else {                                    /* insert middle */
         /* OutOfRangeError check at the beginning */
         struct kit_XorList_Node_Pair pair = {.prev=NULL, .base=NULL};
-        Result result = kit_XorList_Node_Pair_fetch(self, &pair, index);
-        assert(Result_isOk(result));
+        assert(Result_isOk(kit_XorList_Node_Pair_fetch(self, index, &pair)));
         struct kit_XorList_Node *nextNode = pair.base;
         struct kit_XorList_Node *prevNode = pair.prev;
         prevNode->link = kit_XorList_Node_xor(kit_XorList_Node_xor(nextNode, prevNode->link), newNode);
@@ -118,34 +104,26 @@ Result kit_XorList_insert(struct kit_XorList *self, void *e, const size_t index)
 
     self->size += 1;
     self->operationId += 1;
-    return Result_ok(e);
+    return Ok;
 }
 
-Result kit_XorList_pushBack(struct kit_XorList *self, void *e) {
+OneOf(Ok, OutOfMemoryError)
+kit_XorList_pushBack(struct kit_XorList *const self, void *const element) {
     assert(self);
-    return kit_XorList_insert(self, e, self->size);
+    return kit_XorList_insert(self, self->size, element);
 }
 
-Result kit_XorList_pushFront(struct kit_XorList *self, void *e) {
+OneOf(Ok, OutOfMemoryError)
+kit_XorList_pushFront(struct kit_XorList *const self, void *const element) {
     assert(self);
-    return kit_XorList_insert(self, e, 0);
+    return kit_XorList_insert(self, 0, element);
 }
 
-Result kit_XorList_popBack(struct kit_XorList *self) {
-    assert(self);
-    const size_t size = self->size;
-    return size > 0 ? kit_XorList_remove(self, size - 1) : Result_error(&OutOfRangeError);
-}
-
-Result kit_XorList_popFront(struct kit_XorList *self) {
-    assert(self);
-    return kit_XorList_remove(self, 0);
-}
-
-Result kit_XorList_remove(struct kit_XorList *self, const size_t index) {
+ResultOf(void *, OutOfRangeError)
+kit_XorList_remove(struct kit_XorList *const self, const size_t index) {
     assert(self);
     struct kit_XorList_Node_Pair pair = {.prev=NULL, .base=NULL};
-    Result result = kit_XorList_Node_Pair_fetch(self, &pair, index);
+    Result result = kit_XorList_Node_Pair_fetch(self, index, &pair);
 
     if (Result_isOk(result)) {
         struct kit_XorList_Node *prev = pair.prev;
@@ -166,57 +144,94 @@ Result kit_XorList_remove(struct kit_XorList *self, const size_t index) {
         return Result_ok(kit_XorList_Node_delete(node));
     }
 
-    assert(&OutOfRangeError == Result_inspect(result));
+    assert(OutOfRangeError == Result_inspect(result));
     return result;
 }
 
-Result kit_XorList_set(struct kit_XorList *self, void *e, const size_t index) {
+ResultOf(void *, OutOfRangeError)
+kit_XorList_popBack(struct kit_XorList *const self) {
+    assert(self);
+    const size_t size = self->size;
+    return size > 0 ? kit_XorList_remove(self, size - 1) : Result_error(OutOfRangeError);
+}
+
+ResultOf(void *, OutOfRangeError)
+kit_XorList_popFront(struct kit_XorList *const self) {
+    assert(self);
+    return kit_XorList_remove(self, 0);
+}
+
+ResultOf(void *, OutOfRangeError)
+kit_XorList_put(struct kit_XorList *const self, const size_t index, void *const element) {
     assert(self);
     struct kit_XorList_Node_Pair pair = {.prev=NULL, .base=NULL};
-    Result result = kit_XorList_Node_Pair_fetch(self, &pair, index);
+    Result result = kit_XorList_Node_Pair_fetch(self, index, &pair);
 
     if (Result_isOk(result)) {
-        void *oldElement = pair.base->element;
-        pair.base->element = e;
-        Result_ok(oldElement);
+        void *replacedElement = pair.base->element;
+        pair.base->element = element;
+        Result_ok(replacedElement);
     }
 
-    assert(&OutOfRangeError == Result_inspect(result));
+    assert(OutOfRangeError == Result_inspect(result));
     return result;
 }
 
-Result kit_XorList_get(struct kit_XorList *self, const size_t index) {
+ResultOf(void *, OutOfRangeError)
+kit_XorList_get(const struct kit_XorList *const self, const size_t index) {
     assert(self);
     struct kit_XorList_Node_Pair pair = {.prev=NULL, .base=NULL};
-    Result result = kit_XorList_Node_Pair_fetch(self, &pair, index);
+    Result result = kit_XorList_Node_Pair_fetch(self, index, &pair);
 
     if (Result_isOk(result)) {
         Result_ok(pair.base->element);
     }
 
-    assert(&OutOfRangeError == Result_inspect(result));
+    assert(OutOfRangeError == Result_inspect(result));
     return result;
 }
 
-Result kit_XorList_back(struct kit_XorList *self) {
+ResultOf(void *, OutOfRangeError)
+kit_XorList_back(const struct kit_XorList *const self) {
     assert(self);
     const size_t size = self->size;
-    return size > 0 ? kit_XorList_get(self, size - 1) : Result_error(&OutOfRangeError);
+    return size > 0 ? kit_XorList_get(self, size - 1) : Result_error(OutOfRangeError);
 }
 
-Result kit_XorList_front(struct kit_XorList *self) {
+ResultOf(void *, OutOfRangeError)
+kit_XorList_front(const struct kit_XorList *const self) {
     assert(self);
     return kit_XorList_get(self, 0);
 }
 
-size_t kit_XorList_size(struct kit_XorList *self) {
+void
+kit_XorList_clear(struct kit_XorList *const self) {
+    assert(self);
+
+    for (size_t i = self->size; i > 0; i--) {
+        Result_unwrap(kit_XorList_popFront(self));
+    }
+
+    self->operationId += 1;
+}
+
+size_t
+kit_XorList_size(const struct kit_XorList *const self) {
     assert(self);
     return self->size;
 }
 
-bool kit_XorList_isEmpty(struct kit_XorList *self) {
+bool
+kit_XorList_isEmpty(const struct kit_XorList *const self) {
     assert(self);
     return 0 == self->size;
+}
+
+void kit_XorList_delete(struct kit_XorList *self) {
+    if (self) {
+        kit_XorList_clear(self);
+        kit_Allocator_free(self);
+    }
 }
 
 struct kit_XorList_Iterator {
@@ -230,7 +245,8 @@ struct kit_XorList_Iterator {
     struct kit_XorList_Node *next;
 };
 
-Option kit_XorList_Iterator_new(struct kit_XorList *container, enum kit_Bound bound) {
+OptionOf(struct kit_XorList_Iterator *)
+kit_XorList_Iterator_new(struct kit_XorList *const container, const enum kit_Bound bound) {
     assert(container);
     assert(KIT_BOUND_BEGIN <= bound && bound <= KIT_BOUND_END);
     struct kit_XorList_Iterator *self;
@@ -245,17 +261,20 @@ Option kit_XorList_Iterator_new(struct kit_XorList *container, enum kit_Bound bo
     return selfOption;
 }
 
-Option kit_XorList_Iterator_fromBegin(struct kit_XorList *container) {
+OptionOf(struct kit_XorList_Iterator *)
+kit_XorList_Iterator_fromBegin(struct kit_XorList *const container) {
     assert(container);
     return kit_XorList_Iterator_new(container, KIT_BOUND_BEGIN);
 }
 
-Option kit_XorList_Iterator_fromEnd(struct kit_XorList *container) {
+OptionOf(struct kit_XorList_Iterator *)
+kit_XorList_Iterator_fromEnd(struct kit_XorList *const container) {
     assert(container);
     return kit_XorList_Iterator_new(container, KIT_BOUND_END);
 }
 
-void kit_XorList_Iterator_rewind(struct kit_XorList_Iterator *self, enum kit_Bound bound) {
+void
+kit_XorList_Iterator_rewind(struct kit_XorList_Iterator *const self, const enum kit_Bound bound) {
     assert(self);
     assert(self->container);
     assert(KIT_BOUND_BEGIN <= bound && bound <= KIT_BOUND_END);
@@ -273,25 +292,24 @@ void kit_XorList_Iterator_rewind(struct kit_XorList_Iterator *self, enum kit_Bou
     }
 }
 
-void kit_XorList_Iterator_rewindToBegin(struct kit_XorList_Iterator *self) {
+void
+kit_XorList_Iterator_rewindToBegin(struct kit_XorList_Iterator *const self) {
     assert(self);
     kit_XorList_Iterator_rewind(self, KIT_BOUND_BEGIN);
 }
 
-void kit_XorList_Iterator_rewindToEnd(struct kit_XorList_Iterator *self) {
+void
+kit_XorList_Iterator_rewindToEnd(struct kit_XorList_Iterator *const self) {
     assert(self);
     kit_XorList_Iterator_rewind(self, KIT_BOUND_END);
 }
 
-void kit_XorList_Iterator_delete(struct kit_XorList_Iterator *self) {
-    kit_Allocator_free(self);
-}
-
-Result kit_XorList_Iterator_next(struct kit_XorList_Iterator *self) {
+ResultOf(void *, OutOfRangeError, ConcurrentModificationError)
+kit_XorList_Iterator_next(struct kit_XorList_Iterator *const self) {
     assert(self);
 
     if (kit_XorList_Iterator_isModified(self)) {
-        return Result_error(&ConcurrentModificationError);
+        return Result_error(ConcurrentModificationError);
     } else if (self->next) {
         void *nextElement = self->next->element;
         self->last = self->prev = self->next;
@@ -305,15 +323,16 @@ Result kit_XorList_Iterator_next(struct kit_XorList_Iterator *self) {
         }
         return Result_ok(nextElement);
     } else {
-        return Result_error(&OutOfRangeError);
+        return Result_error(OutOfRangeError);
     }
 }
 
-Result kit_XorList_Iterator_previous(struct kit_XorList_Iterator *self) {
+ResultOf(void *, OutOfRangeError, ConcurrentModificationError)
+kit_XorList_Iterator_previous(struct kit_XorList_Iterator *const self) {
     assert(self);
 
     if (kit_XorList_Iterator_isModified(self)) {
-        return Result_error(&ConcurrentModificationError);
+        return Result_error(ConcurrentModificationError);
     } else if (self->prev) {
         void *previousElement = self->prev->element;
         self->last = self->next = self->prev;
@@ -327,61 +346,76 @@ Result kit_XorList_Iterator_previous(struct kit_XorList_Iterator *self) {
         }
         return Result_ok(previousElement);
     } else {
-        return Result_error(&OutOfRangeError);
+        return Result_error(OutOfRangeError);
     }
 }
 
-Result kit_XorList_Iterator_setLast(struct kit_XorList_Iterator *self, void *e) {
+ResultOf(void *, IllegalStateError, ConcurrentModificationError)
+kit_XorList_Iterator_setLast(struct kit_XorList_Iterator *const self, void *const element) {
     assert(self);
 
     if (kit_XorList_Iterator_isModified(self)) {
-        return Result_error(&ConcurrentModificationError);
+        return Result_error(ConcurrentModificationError);
     } else if (self->last) {
-        void *lastElement = self->last->element;
-        self->last->element = e;
-        return Result_ok(lastElement);
+        void *replacedElement = self->last->element;
+        self->last->element = element;
+        return Result_ok(replacedElement);
     } else {
-        return Result_error(&IllegalStateError);
+        return Result_error(IllegalStateError);
     }
 }
 
-bool kit_XorList_Iterator_isModified(struct kit_XorList_Iterator *self) {
+bool
+kit_XorList_Iterator_isModified(const struct kit_XorList_Iterator *const self) {
     assert(self);
     struct kit_XorList *container = self->container;
     return NULL == container || self->operationId != container->operationId;
 }
 
+void
+kit_XorList_Iterator_delete(struct kit_XorList_Iterator *self) {
+    if (self) {
+        kit_Allocator_free(self);
+    }
+}
+
 /*
  * Internals
  */
-Option kit_XorList_Node_new(void *e) {
+OptionOf(struct kit_XorList_Node *)
+kit_XorList_Node_new(void *const element) {
     struct kit_XorList_Node *self;
     Option selfOption = kit_Allocator_calloc(1, sizeof(*self));
 
     if (Option_isSome(selfOption)) {
         self = Option_unwrap(selfOption);
-        self->element = e;
+        self->element = element;
     }
 
     return selfOption;
 }
 
-void *kit_XorList_Node_delete(struct kit_XorList_Node *self) {
+struct kit_XorList_Node *
+kit_XorList_Node_xor(const struct kit_XorList_Node *const p1, const struct kit_XorList_Node *const p2) {
+    return ((struct kit_XorList_Node *) ((intptr_t) p1 ^ (intptr_t) p2));
+}
+
+void *
+kit_XorList_Node_delete(struct kit_XorList_Node *self) {
     assert(self);
     void *e = self->element;
     kit_Allocator_free(self);
     return e;
 }
 
-struct kit_XorList_Node *kit_XorList_Node_xor(struct kit_XorList_Node *p1, struct kit_XorList_Node *p2) {
-    return ((struct kit_XorList_Node *) ((intptr_t) p1 ^ (intptr_t) p2));
-}
-
-Result kit_XorList_Node_Pair_fetch(struct kit_XorList *list, struct kit_XorList_Node_Pair *out, const size_t index) {
+ResultOf(struct kit_XorList_Node_Pair *, OutOfRangeError)
+kit_XorList_Node_Pair_fetch(
+        const struct kit_XorList *const list, const size_t index, struct kit_XorList_Node_Pair *out
+) {
     assert(list);
 
     if (index >= list->size) {
-        return Result_error(&OutOfRangeError);
+        return Result_error(OutOfRangeError);
     }
 
     const bool isFrontCloser = index < list->size / 2;
