c87b7941ea6a44adb9d369146d54a35cbdfd97cf
daddinuz <daddinuz@gmail.com>
2022-04-04 11:43:36 +0200

    fresh start

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 Kit
 ===
-
-[![Build Status](https://travis-ci.org/daddinuz/kit.svg?branch=master)](https://travis-ci.org/daddinuz/kit)
-
-*C Development Kit*
--- valgrind.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env  bash
-
-set -e
-valgrind --trace-children=yes --track-origins=yes --leak-check=full --show-leak-kinds=all --log-file=valgrind.log "${@}"
